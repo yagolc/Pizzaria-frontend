2c898ba1a5fa23feda9d44760e9424c60bc7b5c9
yagolc <yagolimacapistrano@alu.ufc.br>
2023-10-03 22:53:35 -0300

    web finalizado

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -68,7 +68,26 @@ export default function Dashboard({orders}: HomeProps){
         setModalVisible(true);
     }
 
-    
+    async function handleFinishItem(id: string){
+      const apiClient = setupAPIClient();
+      await apiClient.put('/order/finish', {
+        order_id: id,
+      })
+
+      const response = await apiClient.get('/orders')
+
+      setOrderList(response.data)
+      setModalVisible(false)
+    }
+
+    async function handleRefreshOrders() {
+      const apiClient = setupAPIClient();
+
+      const response = await apiClient.get('/orders')
+
+      setOrderList(response.data)
+    }
+
 
     modal.setAppElement('#__next')
 
@@ -84,12 +103,18 @@ export default function Dashboard({orders}: HomeProps){
 
         <div className={styles.containerHeader}>
           <h1>Últimos pedidos</h1>
-          <button>
+          <button onClick={handleRefreshOrders }>
             <FiRefreshCcw size={25} color="#3fffa3"/>
           </button>
         </div>
 
         <article className={styles.listOreders}>
+
+          {orderList.length === 0 && (
+            <span className={styles.emptyList}>
+              Nenhum pedido aberto foi encontrado
+            </span>
+          )}
 
             {orderList.map(item => (
                 <section key={item.id} className={styles.orderItem}> 
@@ -112,6 +137,7 @@ export default function Dashboard({orders}: HomeProps){
         isOpen={modalVisible}
         onRequestClose={handleCloseModal}
         order={modalItem}
+        handleFinishOrder= {handleFinishItem}
         />
       )}
 
